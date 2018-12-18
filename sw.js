be9da60ae28f57a141197a0a8f72641d878f35b2
Hadyan Ahmad <hadyandev@gmail.com>
2018-12-18 09:09:51 +0700

    first commit

---
+++ sw.js
@@ -0,0 +1,49 @@
+importScripts("./node_modules/workbox-sw/build/workbox-sw.js");
+const staticAssets = [
+  "./",
+  "./styles.css",
+  "./app.js",
+  "./fallback.json",
+  "./images/fetch-dog.jpg"
+];
+/*
+const wb = new WorkboxSW();
+wb.precache(staticAssets);
+wb.router.registerRoute('https://newsapi.org/(.*)', wb.strategies.networkFirst());
+console.log('Hello from sw.js');
+*/
+
+self.addEventListener("install", async event => {
+  //console.log(`install`);
+  const cache = await caches.open("news-static");
+  cache.addAll(staticAssets);
+});
+
+self.addEventListener("fetch", event => {
+  //console.log(`event`);
+  const req = event.request;
+  const url = new URL(req.url);
+
+  if (url.origin === location.origin) {
+    event.respondWith(cacheFirst(req));
+  } else {
+    event.respondWith(networkFirst(req));
+  }
+});
+
+async function cacheFirst(req) {
+  const cachedResponse = await caches.match(req);
+  return cachedResponse || fetch(req);
+}
+
+async function networkFirst(req) {
+  const cache = await caches.open("news-dynamic");
+  try {
+    const res = await fetch(req);
+    cache.put(req, res.clone());
+    return res;
+  } catch (error) {
+    const cachedResponse = await cache.match(req);
+    return cachedResponse || (await caches.match("./fallback.json"));
+  }
+}
